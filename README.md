d6f6f878d4463c687cef0976a8c1978ca0f01af9
mnowakowski <mnowakowski@laika.com>
2020-04-02 00:58:33 -0700

    python3 update. add requirements info and better examples.

--- README.md
+++ README.md
@@ -2,42 +2,54 @@
 
 Python program for processing a temporal median filter effect across a set of frames from a video. 
 <br><br>
-**Usage:**
 
-`python temporal_median.py -i "<set_of_frames>" -o "<output_path>" -l <number of frames to process> -offset <length of filter in frames> -simul <frames to process simultaneously> -v <(0 or 1) encode frames into h.264 video>`
+## Installation
+1. Install ffmpeg
+2. Create a python virtual environment
+    ```
+    mkdir venv
+    python3 -m venv venv
+    source venv/bin/activate
+    ```
 
-<br>
-**Surfing Example:**
-<br>
-<br>
-Original:<br>
-<img src="img/gopro_surf_trim.gif" width="500px"/>
+3. Install requirements
+    ```
+    pip install -r requirements.txt
+    ```
+
+4. Run the code!
+
+
+## Usage
+```
+python temporal_median.py -i "<set_of_frames>" -o "<output_path>" -l <number of frames to process> -offset <length of filter in frames> -simul <frames to process simultaneously>
+```
+
+Ex.
+```
+python temporal_median.py -i img/trimmed_saigon_1.mp4 -v
+```
+
+
+## Examples
+### Surfing Example:
+
+Original:
+[<img src="img/gopro_surf_trim.gif" width="500px"/>](https://www.youtube.com/watch?v=LUGksGa4WJA)<br>
 https://www.youtube.com/watch?v=LUGksGa4WJA
-<br>
-<br><br>
-TMF:<br>
-<img src="img/gopro_surf_tmf.gif" width="500px"/>
+TMF:
+[<img src="img/gopro_surf_tmf.gif" width="500px"/>](https://www.youtube.com/watch?v=6K8_iQOxo4w)<br>
 https://www.youtube.com/watch?v=6K8_iQOxo4w
-<br>
 
-<br>
-**Saigon Traffic Example:**
+### Saigon Traffic Example:
 
 Original: https://www.youtube.com/watch?v=8QCsQnr2w4w
 
 TMF: https://www.youtube.com/watch?v=Yhy1uc9s8IU
 
-<br><br>
-**Dependencies:**
-
-[Numpy](http://www.numpy.org/)<br>
-[PIL](http://www.pythonware.com/products/pil/)
 
 
-<br>
-
-**Prior Works:**
-
+## Similar Stuff
 Similar to zo7's median-video (but that one is in C++ and requires OpenCV). 
 https://github.com/zo7/median-video
 
--- temporal_median.py
+++ temporal_median.py
@@ -1,54 +1,88 @@
-import numpy, os, glob
+from __future__ import print_function
+
+import argparse
+import numpy
+import os
+import glob
 import sys
-from PIL import Image
+
 from timeit import default_timer as timer
 from multiprocessing import Process, Pool
-import argparse
+
+
+from PIL import Image
+import imageio
+
+IMAGE_EXTENSIONS = ["*.tiff", "*.tif", "*.jpeg", "*.png", "*.jpg", "*.dng"]
+MOVIE_EXTENSIONS = ["*.mp4", "*.mov"]
+
+
+def get_frame_data(input_data, frame_number):
+    """ Gets the frame with the specified index """
+    if isinstance(input_data, list):
+        return Image.open(input_data[frame_number])
+    return input_data.get_data(frame_number)
+
+
+def get_number_of_frames(input_data):
+    if isinstance(input_data, list):
+        return len(input_data)
+    return round(input_data.get_meta_data()['fps'] * input_data.get_meta_data()['duration']) -1
 
 
 def make_a_glob(root_dir):
+    """ Creates a glob of images from specified path. Checks for JPEG, PNG, TIFF, DNG
+
+    Args:
+        root_dir (str): path to a video or directory of images
+
+    Returns:
+        (list, imageio.core.format.Reader): glob of images from input path
     """
-    Creates a glob of images from specified path. Checks for JPEG, PNG, TIFF, DNG
-    :param root_dir: path to set of images
-    :return: glob of images from input path
-    """
+    # video file, return here
+    if os.path.isfile(root_dir):
+        return imageio.get_reader(root_dir)
+
+    # start hunting for an image sequence
     if not os.path.exists(root_dir):
         raise IOError("No such path: %s" % root_dir)
 
     if not root_dir.endswith("/"):
         root_dir += "/"
 
-    dir_glob = glob.glob(root_dir + "*.tif")
-    extension_list = ["*.tiff", "*.tif", "*.jpeg", "*.png", "*.jpg", "*.dng"]
+    input_data = glob.glob(root_dir + "*.tif")
 
-    for ext in extension_list:
-        if len(dir_glob) == 0:
-            dir_glob = glob.glob(root_dir + ext)
-            if len(dir_glob) == 0:
-                dir_glob = glob.glob(root_dir + ext.upper())
-            if ext == extension_list[(len(extension_list)-1)] and len(dir_glob) == 0:
+    for ext in IMAGE_EXTENSIONS:
+        if len(input_data) == 0:
+            input_data = glob.glob(root_dir + ext)
+            if len(input_data) == 0:
+                input_data = glob.glob(root_dir + ext.upper())
+            if ext == IMAGE_EXTENSIONS[(len(IMAGE_EXTENSIONS)-1)] and len(input_data) == 0:
                 raise IOError("No images found in directory: %s" % root_dir)
         else:
             break
 
-    print "First image is: " + dir_glob[0]
-    print "Number of frames found: ", len(dir_glob)
-    return dir_glob
+    print("First image is: " + input_data[0])
+    print("Number of frames found: ", len(input_data))
+    return input_data
 
 
 def get_frame_limit(limit_frames, globsize):
-    """
-    Determines a limit on the number of frames
-    :param limit_frames:
-    :param globsize:
-    :return:
+    """ Determines a limit on the number of frames
+
+    Args:
+        limit_frames:
+        globsize:
+
+    Returns:
+        int: total frames to run TMF on
     """
     if limit_frames != -1:
         if globsize > limit_frames:
             total_frames = limit_frames
-            print "Frames limited to ", limit_frames
+            print("Frames limited to ", limit_frames)
         else:
-            print "Frame limit of ", limit_frames, "is higher than total # of frames: ", globsize
+            print("Frame limit of ", limit_frames, "is higher than total # of frames: ", globsize)
             total_frames = globsize
     else:
         total_frames = globsize
@@ -57,10 +91,13 @@ def get_frame_limit(limit_frames, globsize):
 
 
 def make_output_dir(output_dir):
-    """
-    Creates uniquely-named new folder along specified path
-    :param output_dir: output path
-    :return: path to new folder
+    """ Creates uniquely-named new folder along specified path
+
+    Args:
+        output_dir: output path
+
+    Returns:
+        str: path to new folder
     """
     if not output_dir.endswith("/"):
         output_dir += "/"
@@ -74,25 +111,28 @@ def make_output_dir(output_dir):
 
     os.mkdir(output_path + "tmf" + str(slitscan_current) + "/")
     frame_path = output_path + "tmf" + str(slitscan_current) + "/"
-    print "Made directory: ", frame_path
+    print("Made directory: ", frame_path)
     return frame_path
 
 
-def do_sizing(dir_glob):
-    first = Image.open(dir_glob[0])
-    width, height = first.size
-    print "width is: ", width," height is: ", height
+def do_sizing(input_data):
+    if isinstance(input_data, list):
+        first = Image.open(input_data[0])
+        width, height = first.size
+        print("width is: ", width, " height is: ", height)
+    else:
+        return input_data.get_meta_data()['size']
     return width, height
 
 
 # handy code by Vladimir Ignatyev, found here: https://gist.github.com/vladignatyev/06860ec2040cb497f0f3
 def progress(count, total, suffix=''):
-    """
-    Creates and displays a progress bar in console log.
-    :param count: parts completed
-    :param total: parts to complete
-    :param suffix: any additional descriptors
-    :return:
+    """ Creates and displays a progress bar in console log.
+
+    Args:
+        count: parts completed
+        total: parts to complete
+        suffix: any additional descriptors
     """
     bar_len = 60
     filled_len = int(round(bar_len * count / float(total)))
@@ -104,70 +144,7 @@ def progress(count, total, suffix=''):
     sys.stdout.flush()
 
 
-def temporal_median_filter(dir_glob, output_dir, limit_frames, output_format):
-    frame_path = make_output_dir(output_dir)
-    width, height = do_sizing(dir_glob)
-    total_frames = get_frame_limit(limit_frames, len(dir_glob))
-
-    print "\n"
-
-    # for each iteration:
-    #   delete oldest frame.
-    #   add new frame
-    #   do median calculations
-    #   write to filtered_array, save result.
-
-    frame_offset = 8
-    whole_array = numpy.zeros((frame_offset+frame_offset+1, height, width, 3), numpy.uint8)
-
-    for frame_number in xrange(frame_offset+frame_offset+1):
-        next_im = Image.open(dir_glob[frame_number])
-        next_array = numpy.array(next_im, numpy.uint8)
-        del next_im
-        whole_array[frame_number, :, :, :] = next_array
-        del next_array
-        progress(frame_number, total_frames)
-
-    median_array = whole_array[0:frame_offset+frame_offset+1, :, :, :]
-
-    for frame_number in range(total_frames):
-        start = timer()
-        if frame_number == 0:
-            pass
-        elif (frame_number + frame_offset) >= total_frames:
-            median_array = median_array[1:,:,:,:]
-        else:
-            next_im = Image.open(dir_glob[frame_offset+frame_offset+frame_number])
-            next_array = numpy.array(next_im, numpy.uint8)
-            median_array = numpy.roll(median_array, -1, axis=0)
-            median_array[frame_offset+frame_offset, :, :, :] = next_array
-
-        # low = frame_number - frame_offset
-        # high = frame_number + frame_offset
-        # if frame_number-frame_offset <= 0:
-        #     low = 0
-        # if frame_number+frame_offset+frame_offset >= total_frames:
-        #     high = total_frames
-        #
-        # print low, high
-        filtered_array = numpy.zeros((height, width, 3), numpy.uint8)
-
-        start2 = timer()
-        filtered_array[:, :, 0] = numpy.median(median_array[:,:,:,0], axis=0)
-        filtered_array[:, :, 1] = numpy.median(median_array[:,:,:,1], axis=0)
-        filtered_array[:, :, 2] = numpy.median(median_array[:,:,:,2], axis=0)
-        end2 = timer()
-        print "Median array calc: %s" % (end2-start2)
-
-        img = Image.fromarray(filtered_array)
-        frame_name = frame_path + str(frame_number) + "." + output_format
-        img.save(frame_name, format=output_format)
-        progress(frame_number, total_frames)
-        end = timer()
-        print "Time required: %s seconds" % (end-start)
-
-
-def temporal_median_filter_multi2(dir_glob, output_dir, limit_frames, output_format, frame_offset=8, simultaneous_frames=8):
+def temporal_median_filter_multi2(input_data, output_dir, limit_frames, output_format, frame_offset=8, simultaneous_frames=8):
     """
     Uses multiprocessing to efficiently calculate a temporal median filter across set of input images.
 
@@ -180,18 +157,20 @@ def temporal_median_filter_multi2(dir_glob, output_dir, limit_frames, output_for
     randframes_----0      |--f.o----|s.o|---f.o---|
 
 
-    :param dir_glob: globbed input directory
-    :param output_dir: path to output
-    :param limit_frames: put a limit on the number of frames
-    :param output_format: select PNG, TIFF, or JPEG (default)
-    :param frame_offset: Number of frames to use for median calculation
-    :param simultaneous_frames: Number of frames to process simultaneously
-    :return:
+    Args:
+        input_data: globbed input directory
+        output_dir: path to output
+        limit_frames: put a limit on the number of frames
+        output_format: select PNG, TIFF, or JPEG (default)
+        frame_offset: Number of frames to use for median calculation
+        simultaneous_frames: Number of frames to process simultaneously
+    Returns:
+        str: path to final frames
     """
     start2 = timer()
     frame_path = make_output_dir(output_dir)
-    width, height = do_sizing(dir_glob)
-    total_frames = get_frame_limit(limit_frames, len(dir_glob))
+    width, height = do_sizing(input_data)
+    total_frames = get_frame_limit(limit_frames, get_number_of_frames(input_data))
 
     median_array = numpy.zeros((frame_offset+simultaneous_frames+frame_offset, height, width, 3),numpy.uint8)
 
@@ -199,8 +178,8 @@ def temporal_median_filter_multi2(dir_glob, output_dir, limit_frames, output_for
         median_array[frame, :, :, :] = numpy.random.randint(low=0, high=255, size=(height, width, 3))
 
     # read all the frames into big ol' array
-    for frame_number in xrange(simultaneous_frames+frame_offset):
-        next_im = Image.open(dir_glob[frame_number])
+    for frame_number in range(simultaneous_frames+frame_offset):
+        next_im = get_frame_data(input_data, frame_number)
         next_array = numpy.array(next_im, numpy.uint8)
         del next_im
         median_array[frame_offset+frame_number, :, :, :] = next_array
@@ -223,7 +202,7 @@ def temporal_median_filter_multi2(dir_glob, output_dir, limit_frames, output_for
                 if (current_frame+frame_offset+x) > total_frames:
                     next_array = numpy.random.randint(low=0, high=255, size=(height, width, 3))
                 else:
-                    next_im = Image.open(dir_glob[frame_offset+current_frame+x])
+                    next_im = get_frame_data(input_data, frame_offset+current_frame+x)
                     next_array = numpy.array(next_im, numpy.uint8)
                 median_array[frame_offset+frame_offset+x, :, :, :] = next_array
 
@@ -248,15 +227,27 @@ def temporal_median_filter_multi2(dir_glob, output_dir, limit_frames, output_for
         current_frame += simultaneous_frames
 
     end2 = timer()
-    print "\nTotal Time was: %.02f sec. %.02f sec per frame." % (end2-start2, ((end2-start2)/total_frames))
+    print("\nTotal Time was: %.02f sec. %.02f sec per frame." % (end2-start2, ((end2-start2)/total_frames)))
     return frame_path
 
 
 def median_calc(median_array):
-    return numpy.median(median_array[:,:,:,0], axis=0), numpy.median(median_array[:,:,:,1], axis=0), numpy.median(median_array[:,:,:,2], axis=0)
+    return numpy.median(median_array[:, :, :, 0], axis=0), \
+           numpy.median(median_array[:, :, :, 1], axis=0), \
+           numpy.median(median_array[:, :, :, 2], axis=0)
 
 
 def make_a_video(output_dir, output_format, name):
+    """ Use ffmpeg to make a video out of our cool frames
+    
+    Args:
+        output_dir (str): 
+        output_format (str): 
+        name (str): 
+
+    Returns:
+        None
+    """
     if not output_dir.endswith("/"):
         output_dir += "/"
     os.system('ffmpeg -r 24 -i ' + output_dir + '%d.' + output_format + ' -c:v libx264 ' + output_dir + name)
@@ -268,17 +259,27 @@ if __name__ == '__main__':
 
     parser.add_argument("-i", "--input_dir",
                         help="Input directory. Set of frames or a single video. Tests for frames first.", required=True)
-    parser.add_argument("-o", "--output_dir", default="None",
+    parser.add_argument("-o", "--output_dir", default=os.getcwd(),
                         help="Path for output frames (optional) Default: Subdir of input)")
-    parser.add_argument("-offset", "--frame_offset", default=8, type=int, help="Number of Frames to use for TMF (optional)")
+    parser.add_argument("-offset", "--frame_offset", default=8, type=int,
+                        help="Number of Frames to use for TMF (optional)")
     parser.add_argument("-l", "--frame_limit", default=-1, type=int,
                         help="Limit number of frames to specified int (optional)")
     parser.add_argument("-format", "--output_format", default="JPEG", help="Output image format. (optional)")
     parser.add_argument("-simul", "--simultaneous_frames",type=int, default="8",
                         help="Number of frames to process on each iteration (faster performance using more cores)")
-    parser.add_argument("-v", "--video", default=0,
-                        help="Optional: Encode h.264 video of resulting frames (0 or 1. Default is 0)")
+    parser.add_argument("-v", "--video", action="store_true", default=False, dest="video",
+                        help="Optional: Encode h.264 video of resulting frames. Defaults to False.")
 
     args = parser.parse_args()
-    output_path = temporal_median_filter_multi2(make_a_glob(args.input_dir), args.output_dir, args.frame_limit, args.output_format, args.frame_offset, args.simultaneous_frames)
-    make_a_video(output_path, args.output_format, "TMF.mp4")
+    output_path = temporal_median_filter_multi2(
+        make_a_glob(args.input_dir),
+        args.output_dir,
+        args.frame_limit,
+        args.output_format,
+        args.frame_offset,
+        args.simultaneous_frames
+    )
+
+    if args.video:
+        make_a_video(output_path, args.output_format, "TMF.mp4")
